d763e1de78aa9d069eb884db306300f9c16d7d3e
jubaan <julio.ab@outlook.com>
2020-01-20 13:56:29 -0600

    FEAT: methods definition
    Initialized methods needed for the project
    
    - #my_each
    - #my_each_with_index
    - #my_select
    - #my_all?
    - #my_any?
    - #my_none?
    - #my_count
    - #my_map
    - #my_inject
    - #my_inject
    
    Changes to be committed:
    	deleted:    change_file_name.rb
    	new file:   enumerable-methods.rb

---
+++ enumerable-methods.rb
@@ -0,0 +1,34 @@
+# Project 2: Enumerable Methods
+
+module Enumerable
+  def my_each
+  end
+
+  def my_each_with_index
+  end
+
+  def my_select
+
+  end
+
+  def my_all?
+  end
+
+  def my_any?
+  end
+
+  def my_none?
+  end
+
+  def my_count
+  end
+
+  def my_map
+  end
+
+  def my_inject
+  end
+
+  def multiply_els(arr)
+  end
+end
