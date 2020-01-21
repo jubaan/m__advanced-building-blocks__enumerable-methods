574e1001b41d7cde6fdb20b3047a2e54c41dc368
jubaan <julio.ab@outlook.com>
2020-01-21 13:31:30 -0600

    Feat: definition of my_each method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,33 +1,37 @@
 # Project 2: Enumerable Methods
 
 module Enumerable
-  def my_each
+  def my_each(&block)
+    arr.length.times do |count|
+      puts yield(arr[count])
+    end
+    arr
   end
 
-  def my_each_with_index
-  end
+  # def my_each_with_index
+  # end
 
-  def my_select
-  end
+  # def my_select
+  # end
 
-  def my_all?
-  end
+  # def my_all?
+  # end
 
-  def my_any?
-  end
+  # def my_any?
+  # end
 
-  def my_none?
-  end
+  # def my_none?
+  # end
 
-  def my_count
-  end
+  # def my_count
+  # end
 
-  def my_map
-  end
+  # def my_map
+  # end
 
-  def my_inject
-  end
+  # def my_inject
+  # end
 
-  def multiply_els(arr)
-  end
+  # def multiply_els(arr)
+  # end
 end
