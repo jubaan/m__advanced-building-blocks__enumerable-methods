092aee98e4bd228b70b8b3d5bfaab183164138e2
jubaan <julio.ab@outlook.com>
2020-01-22 11:01:06 -0600

    Fix: my_each change yield for proc.call

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,40 +1,10 @@
 # Project 2: Enumerable Methods
 
 module Enumerable
-
-  public
-
-  def my_each
+  def my_each(&proc)
     length.times do |i|
-      yield(self[i])
+      proc.call(self[i])
     end
     self
   end
-
-  # def my_each_with_index
-  # end
-
-  # def my_select
-  # end
-
-  # def my_all?
-  # end
-
-  # def my_any?
-  # end
-
-  # def my_none?
-  # end
-
-  # def my_count
-  # end
-
-  # def my_map
-  # end
-
-  # def my_inject
-  # end
-
-  # def multiply_els(arr)
-  # end
 end
