634b8b07d5c809bac819c7d6706d3abe46a2dd10
jubaan <julio.ab@outlook.com>
2020-01-22 20:02:19 -0600

    Fix: Refactor:  my_each, my_each_with_index

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,63 +1,65 @@
 # Project 2: Enumerable Methods
-
+require 'byebug'
 module Enumerable
   def my_each(&proc)
-    length.times do |i|
-      proc.call(self[i])
+    if block_given?
+      length.times { |i| proc.call(self[i]) }
+      return self
     end
-    self
+    to_enum
   end
 
   def my_each_with_index(&proc)
-    length.times do |item, _i|
-      proc.call(item)
+    if block_given?
+      length.times { |item, _idx| proc.call(item) }
+      return self
     end
-    self
+    to_enum
   end
 
-  def my_select(&proc)
-    selection = []
-    length.times do |i|
-      selection.push(self[i]) if proc.call(self[i])
-    end
-    selection
-  end
+  # def my_select(&proc)
+  #   selection = []
+  #   length.times do |i|
+  #     selection.push(self[i]) if proc.call(self[i])
+  #   end
+  #   selection
+  # end
 
-  def my_all?(&proc)
-    length.times do |i|
-      return false if proc.call(self[i]) == false
-    end
-  end
+  # def my_all?(&proc)
+  #   length.times do |i|
+  #     return false if proc.call(self[i]) == false
+  #   end
+  # end
 
-  def my_any?(&proc)
-    length.times do |i|
-      return true if proc.call(self[i]) == true
-    end
-  end
+  # def my_any?(&proc)
+  #   length.times do |i|
+  #     return true if proc.call(self[i]) == true
+  #   end
+  # end
 
-  def my_none?(&proc)
-    length.times do |i|
-      return true if proc.call(self[i]) != true
-    end
-  end
+  # def my_none?(&proc)
+  #   length.times do |i|
+  #     return true if proc.call(self[i]) != true
+  #   end
+  # end
 
-  def my_map(&proc)
-    my_each do |i|
-      self[i] = proc.call(self[i])
-    end
-    self
-  end
+  # def my_map(&proc)
+  #   my_each do |i|
+  #     self[i] = proc.call(self[i])
+  #   end
+  #   self
+  # end
 
-  def my_inject(*arr)
-    init = arr.size.positive?
-    result = init ? arr[0] : self[0]
-    drop(init ? 0 : 1).my_each do |item|
-      result = proc.call(result, item)
-    end
-    result
-  end
+  # def my_inject(*arr)
+  #   init = arr.size.positive?
+  #   result = init ? arr[0] : self[0]
+  #   drop(init ? 0 : 1).my_each do |item|
+  #     result = proc.call(result, item)
+  #   end
+  #   result
+  # end
 
-  def multiply_els(*arr)
-    my_inject { |x, y| x * y }
-  end
+  # def multiply_els(*arr)
+  #   my_inject { |x, y| x * y }
+  # end
 end
