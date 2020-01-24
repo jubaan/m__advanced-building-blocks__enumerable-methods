ad970fb9f88966059102a74a522b58992f96c7e1
jubaan <julio.ab@outlook.com>
2020-01-23 20:01:28 -0600

    Fix: Refactor: my_each, my_each_with_index and my_select they all return its class if no block is given

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,76 +1,19 @@
 # Project 2: Enumerable Methods
-require 'byebug'
 module Enumerable
-  def my_each(&proc)
-    if block_given?
-      length.times { |i| proc.call(self[i]) }
-      return self
-    end
+  def my_each(&block)
+    length.times { |i| block.call(self[i]) } if block_given?
     to_enum
   end
 
-  def my_each_with_index(&proc)
-    if block_given?
-      length.times { |item, _idx| proc.call(item) }
-      return self
-    end
+  def my_each_with_index(&block)
+    my_each { |item| block.call(item) } if block_given?
     to_enum
   end
 
-  def my_select(&proc)
-    if block_given?
-      selection = []
-      length.times do |i|
-        selection.push(self[i]) if proc.call(self[i])
-      end
-      return self
-    end
-    to_enum
+  def my_select(&block)
+    to_enum unless block_given?
+    selection = []
+    my_each { |item| selection << item if block.call(item) }
+    selection
   end
-
-  def my_all?(&proc)
-    switch = true
-    if block_given?
-      length.times do |i|
-        switch = false if proc.call(self[i]) == false
-      end
-    end
-    switch
-  end
-
-  # def my_any?(&proc)
-  #   length.times do |i|
-  #     return true if proc.call(self[i]) == true
-  #   end
-  # end
-
-  # def my_none?(&proc)
-  #   length.times do |i|
-  #     return true if proc.call(self[i]) != true
-  #   end
-  # end
-
-  def my_map(&proc)
-    if block_given?
-      mapped = []
-      length.times do |i|
-        mapped.push(proc.call(self[i]))
-      end
-      return mapped
-    end
-    to_enum
-  end
-
-  def my_inject(accumulator, operator = nil, &block)
-    block ||= ->(acc, value) { acc.send(operator, value) }
-
-    my_each do |value|
-      accumulator = block.call(accumulator, value)
-    end
-    accumulator
-  end
-
-  # def multiply_els(*arr)
-  #   my_inject { |x, y| x * y }
-  # end
 end
