e0f6a81f2591e33fa0d66ff79ddfa5801e04b954
jubaan <julio.ab@outlook.com>
2020-01-24 10:34:57 -0600

    Fix: to_enum first element of al methods

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,13 +1,15 @@
+require 'minitest/pride'
+
 # Project 2: Enumerable Methods
 module Enumerable
   def my_each(&block)
-    length.times { |i| block.call(self[i]) } if block
     to_enum
+    length.times { |i| block.call(self[i]) } and self if block
   end
 
   def my_each_with_index(&block)
-    my_each { |item| block.call(item) } if block
     to_enum
+    my_each { |item| block.call(item) } if block
   end
 
   def my_select(&block)
@@ -18,8 +20,12 @@ module Enumerable
   end
 
   def my_map(&block)
+    to_enum
     mapped = []
     my_each { |item| mapped << block.call(item) } and return mapped if block
-    to_enum
   end
 end
+
+
+array = [1,2,5,8,7,98,4]
+p array.my_each_with_index {|x| x * 2}
